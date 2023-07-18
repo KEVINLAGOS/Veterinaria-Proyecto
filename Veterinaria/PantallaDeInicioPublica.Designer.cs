753c12a4b016e31a78704f03281d8ed0352d5541
Kevin Lagos <kevinlagos977@gmail.com>
2023-07-18 13:50:48 -0500

    agregando veterinaria

---
+++ Veterinaria/PantallaDeInicioPublica.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Veterinaria
+{
+    partial class PantallaDeInicioPublica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnServicios = new System.Windows.Forms.Button();
+            this.btnCita = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnAcerca = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(149)))), ((int)(((byte)(20)))));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1160, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veterinaria";
+            // 
+            // btnServicios
+            // 
+            this.btnServicios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnServicios.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnServicios.FlatAppearance.BorderSize = 4;
+            this.btnServicios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnServicios.Location = new System.Drawing.Point(104, 155);
+            this.btnServicios.Name = "btnServicios";
+            this.btnServicios.Size = new System.Drawing.Size(129, 48);
+            this.btnServicios.TabIndex = 1;
+            this.btnServicios.Text = "Servicios";
+            this.btnServicios.UseVisualStyleBackColor = false;
+            this.btnServicios.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCita
+            // 
+            this.btnCita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnCita.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnCita.FlatAppearance.BorderSize = 4;
+            this.btnCita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCita.Location = new System.Drawing.Point(334, 155);
+            this.btnCita.Name = "btnCita";
+            this.btnCita.Size = new System.Drawing.Size(129, 48);
+            this.btnCita.TabIndex = 2;
+            this.btnCita.Text = "Cita";
+            this.btnCita.UseVisualStyleBackColor = false;
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnProductos.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnProductos.FlatAppearance.BorderSize = 4;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductos.Location = new System.Drawing.Point(601, 155);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(129, 48);
+            this.btnProductos.TabIndex = 3;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = false;
+            // 
+            // btnAcerca
+            // 
+            this.btnAcerca.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnAcerca.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(211)))));
+            this.btnAcerca.FlatAppearance.BorderSize = 4;
+            this.btnAcerca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcerca.Location = new System.Drawing.Point(812, 141);
+            this.btnAcerca.Name = "btnAcerca";
+            this.btnAcerca.Size = new System.Drawing.Size(233, 78);
+            this.btnAcerca.TabIndex = 4;
+            this.btnAcerca.Text = "Acerca De Nosotros";
+            this.btnAcerca.UseVisualStyleBackColor = false;
+            // 
+            // PantallaDeInicioPublica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(237)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(1160, 661);
+            this.Controls.Add(this.btnAcerca);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.btnCita);
+            this.Controls.Add(this.btnServicios);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "PantallaDeInicioPublica";
+            this.Text = "Inicio Publico";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnServicios;
+        private System.Windows.Forms.Button btnCita;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnAcerca;
+    }
+}

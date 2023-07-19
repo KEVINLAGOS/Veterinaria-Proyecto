8230c29046cb1a1bd99fbab87f7bdaf19799e1eb
Jaqueline Cruz <jackcrzob20@gmail.com>
2023-07-19 08:49:09 -0500

    creacion de la clase conexionBD

---
+++ Veterinaria/ConexionBD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Veterinaria
+{
+    public class ConexionBD
+    {
+        private static string connectionString = "Data Source=DESKTOP-9GCKMTO\\SQLEXPRESS;Initial Catalog=VETERINARIA;Integrated Security=True;";
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        public static void AbrirConexion()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public static void CerrarConexion()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return connection;
+        }
+    }
+
+}
